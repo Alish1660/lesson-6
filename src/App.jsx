0fdc758a874a66c5529436084bac81c8c8dcb171
Alish1660 <git>
2024-05-27 23:32:45 +0500

    first

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Login } from "./components/login/Login";
+import { Cars } from "./components/cars/Cars";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Login />
+
+      <Cars />
+    </>
+  );
+}
+
+export default App;
